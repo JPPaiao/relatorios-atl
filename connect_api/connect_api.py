3fb9455a31816a1743a0c65f2bacf84b14527314
jp-atlantis <jpaiao@atlantis-ssz.com.br>
2025-01-17 17:07:22 -0300

    refactor: Adicionado o modo de token via servidor, que não precisa se authenticar varias vezes

--- connect_api/connect_api.py
+++ connect_api/connect_api.py
@@ -1,16 +1,11 @@
 import os.path
-# import webbrowser
-from google.auth.exceptions import RefreshError
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2 import service_account
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 from dotenv import load_dotenv
 
 load_dotenv()
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
-
+SERVICE_ACCOUNT_FILE = 'connect_api/server_key.json'
 HOMOLOG = {
   'nav': os.getenv('ID_HOMOLOG'),
   'gja': os.getenv('ID_HOMOLOG'),
@@ -19,58 +14,11 @@ HOMOLOG = {
 
 SAMPLE_RANGE_NAME = "Página1" 
 
-# def main():
-#   creds = None
-
-#   if os.path.exists("connect_api/token.json"):
-#   # if os.path.exists("/var/www/geradorrelatorio/connect_api/token.json"):
-#     creds = Credentials.from_authorized_user_file("connect_api/token.json", SCOPES)
-#     # creds = Credentials.from_authorized_user_file("/var/www/geradorrelatorio/connect_api/token.json", SCOPES)
-#   if not creds or not creds.valid:
-#     if creds and creds.expired and creds.refresh_token:
-#       creds.refresh(Request())
-#     else:
-#       flow = InstalledAppFlow.from_client_secrets_file(
-#           "connect_api/client_secret.json", SCOPES
-#           # "/var/www/geradorrelatorio/connect_api/client_secret.json", SCOPES
-#       )
-#       creds = flow.run_local_server(port=0)
-def main():
-  creds = None
-  token_path = "connect_api/token.json"
-  client_secret_path = "connect_api/client_secret.json"
-
-  # Carregar o token existente, se disponível
-  if os.path.exists(token_path):
-    creds = Credentials.from_authorized_user_file(token_path, SCOPES)
-
-  # Verificar se as credenciais são válidas
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      try:
-        creds.refresh(Request())
-      except RefreshError:
-        creds = None
-    if not creds:
-      if not os.path.exists(client_secret_path):
-        raise FileNotFoundError("Arquivo client_secret.json não encontrado.")
-      
-      # Executa o fluxo de autenticação para obter novas credenciais
-      flow = InstalledAppFlow.from_client_secrets_file(client_secret_path, SCOPES)
-      creds = flow.run_local_server(port=0)
-
-    # try:
-    #   creds = flow.run_local_server(port=0)
-    # except webbrowser.Error:
-    #   webbrowser.register('firefox', None, webbrowser.BackgroundBrowser("/usr/bin/firefox"))
-    #   creds = flow.run_local_server(port=0, open_browser=True)
-
-
-    with open("connect_api/token.json", "w") as token:
-    # with open("/var/www/geradorrelatorio/connect_api/token.json", "w") as token:
-      token.write(creds.to_json())
-
-  service = build("sheets", "v4", credentials=creds)
+def main():  
+  credentials = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE, scopes=SCOPES
+  )
+  service = build("sheets", "v4", credentials=credentials)
   sheet = service.spreadsheets()
 
   return {
